aebf33a95c7b6e13a491b7ec19e7cac5b33ec0a5
Mamy Nekena <antso.2@mail.hei.school>
2026-08-20 16:25:44 +0300

    initial commit

---
+++ src/main/java/school/hei/exampoowithia/ExamPooWithIaApplication.java
@@ -0,0 +1,13 @@
+package school.hei.exampoowithia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPooWithIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPooWithIaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/hei/exampoowithia/ExamPooWithIaApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.exampoowithia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPooWithIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
